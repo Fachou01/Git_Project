eddc1e551954003e4a9f1a4583982909be0ba2a1
mohamed koumenji <koumenjimohamed@gmail.com>
2023-01-28 15:18:59 +0100

    adding class person

---
+++ src/main/java/tn/esprit/alternance/Person.java
@@ -0,0 +1,44 @@
+package tn.esprit.alternance;
+
+public class Person {
+	public int id;
+	public String nom;
+	public String prenom;
+	
+	public Person() {}
+	
+	public Person(int id,String nom,String prenom) {
+		this.id=id;
+		this.nom = nom;
+		this.prenom=prenom;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	
+	
+	
+	
+	
+}
