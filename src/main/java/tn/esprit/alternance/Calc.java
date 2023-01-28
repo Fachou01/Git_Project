e8b3907d7f46b7e55495f5c37ad9ebfe32629b75
seif ayadi <seif.ayadi.3.9.2@gmail.com>
2023-01-28 15:40:35 +0100

    seif modif

---
+++ src/main/java/tn/esprit/alternance/Calc.java
@@ -0,0 +1,21 @@
+package tn.esprit.alternance;
+
+public class Calc {
+
+	
+	private int id;
+	private int somme;
+	public void calc(int id) {
+		this.id=id;
+		this.somme=somme;
+	}
+	
+	public int calcule(int id) {
+		for (int i=0;i<10;i++) {
+			
+		 somme=somme*i;
+		}
+		return somme;
+	}
+	
+}
