16a041ae3bfed6fed89e0cc89f528bb06d90695c
YessinRebhi <rebhi.yessin@esprit.tn>
2023-02-15 10:57:16 +0100

    finalazing project, adding some products and testing

--- src/main/java/tn/esprit/alternance/CallRestWebService.java
+++ src/main/java/tn/esprit/alternance/CallRestWebService.java
@@ -9,5 +9,9 @@ public class CallRestWebService {
 		System.out.println(g1);
 		Person p1 = new Person(1,"Koumenji","Mohamed");
 		System.out.println(p1);
+		Product product1 = new Product("yaourt",0.400,1);
+		System.out.println(product1);
+		Product product2 = new Product("coca",2000,2);
+		System.out.println(product1);
 	}
 }
