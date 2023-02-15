a44cb2bb1682ce432708be0bf7da14becb4519b2
seif ayadi <seif.ayadi.3.9.2@gmail.com>
2023-02-15 10:36:33 +0100

    ajout de puissance dans main

--- src/main/java/tn/esprit/alternance/CallRestWebService.java
+++ src/main/java/tn/esprit/alternance/CallRestWebService.java
@@ -7,5 +7,7 @@ public class CallRestWebService {
 	public static void main(String[] args) {
 		Gamers g1 = new Gamers(1252,"Stow",99,99);
 		System.out.println(g1);
+		Puissance p1 =new Puissance();
+		p1.calcule(4, 5);
 	}
 }
--- src/main/java/tn/esprit/alternance/Puissance.java
+++ src/main/java/tn/esprit/alternance/Puissance.java
@@ -9,12 +9,8 @@ public class Puissance {
 
 
 
-public Puissance(int id,int id1,int somme) {
-	
-	this.id=id;
-	
-	this.somme=somme;
-	this.id1=id1;
+public Puissance() {
+
 }
 
 public void calcule(int id,int id1) {
