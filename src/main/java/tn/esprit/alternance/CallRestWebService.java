e12134224a61321233dd3fc8a1d268ae411bd389
mohamed koumenji <koumenjimohamed@gmail.com>
2023-02-15 10:03:02 +0100

    feature/koumenji - calling person class and initiated in the main method

--- src/main/java/tn/esprit/alternance/CallRestWebService.java
+++ src/main/java/tn/esprit/alternance/CallRestWebService.java
@@ -7,5 +7,7 @@ public class CallRestWebService {
 	public static void main(String[] args) {
 		Gamers g1 = new Gamers(1252,"Stow",99,99);
 		System.out.println(g1);
+		Person p1 = new Person(1,"Koumenji","Mohamed");
+		System.out.println(p1);
 	}
 }
